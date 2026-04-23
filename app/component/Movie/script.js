d76dae1b14acec50c4056b0d573f2beb2e332192
Beau-Nimis Leonie <beaunimis1@mmi-dev.unilim.fr>
2026-04-23 10:52:19 +0200

    update credentials

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,9 +1,12 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
 let Movie = {};
 
 Movie.format =  async function (movies) {
 
   data=[]
-  
+
   if (!movies || movies.length === 0) {
     let response = await fetch("./component/Movie/template-message.html");
     return await response.text();
