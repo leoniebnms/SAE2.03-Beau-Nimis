ec0aa2bc5508f292e9521f81f257b5f19cc6e532
Beau-Nimis Leonie <beaunimis1@mmi-dev.unilim.fr>
2026-04-23 10:50:00 +0200

    update credentials

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,19 +1,27 @@
 let Movie = {};
 
 Movie.format =  async function (movies) {
-let templateFile = await fetch("./component/Movie/template.html");
-let template = await templateFile.text();
 
-  let menuItems = "";
-  for (let movie of movies) {
-    let item = template;
-
-
-    item = item.replace("{{image}}", movie.image);
-    item = item.replaceAll("{{name}}", movie.name);
-    menuItems += item;
+  data=[]
+  
+  if (!movies || movies.length === 0) {
+    let response = await fetch("./component/Movie/template-message.html");
+    return await response.text();
   }
-  return menuItems;
+
+//   let response = await fetch("./component/Movie/template.html");
+//   let template = await response.text();
+
+//   let menuItems = "";
+//   for (let movie of movies) {
+//     let item = template;
+
+
+//     item = item.replace("{{image}}", movie.image);
+//     item = item.replaceAll("{{name}}", movie.name);
+//     menuItems += item;
+//   }
+//   return menuItems;
 }
 
 export { Movie };
